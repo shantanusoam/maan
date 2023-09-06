9f0f2b1a9802790c46b3531edcea56af6e2b2b33
shantanusoam <bluestackssoam@gmail.com>
2023-09-06 10:23:13 +0530

    new_user created

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,25 +1,26 @@
-// import { prisma } from '@/utils/db'
-import { currentUser } from '@clerk/nextjs'
+import { prisma } from '@/utils/db'
+import {  currentUser } from '@clerk/nextjs'
 import { redirect } from 'next/navigation'
 
 const createNewUser = async () => {
   const user = await currentUser()
   console.log(user)
 
-  // const match = await prisma.user.findUnique({
-  //   where: {
-  //     clerkId: user.id as string,
-  //   },
-  // })
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user.id as string,
+    },
+  })
 
-  // if (!match) {
-  //   await prisma.user.create({
-  //     data: {
-  //       clerkId: user.id,
-  //       email: user?.emailAddresses[0].emailAddress,
-  //     },
-  //   })
-  // }
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user.id,
+        email: user?.emailAddresses[0].emailAddress,
+      },
+    })
+  }
+
 
   redirect('/journal')
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
-import Image from 'next/image'
+import { auth } from '@clerk/nextjs'
 import Link from 'next/link'
 
-export default function Home() {
-  return (    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
+export default async function Home() {
+  const { userId } = await auth()
+  let href = userId ? '/journal' : '/new-user'
+
+  return (
+    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
       <div className="w-full max-w-[600px] mx-auto">
         <h1 className="text-6xl mb-4">The best Journal app, period.</h1>
         <p className="text-2xl text-white/60 mb-4">
@@ -10,12 +14,13 @@ export default function Home() {
           you have to do is be honest.
         </p>
         <div>
-          <Link href='/'>
+          <Link href={href}>
             <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
               get started
             </button>
           </Link>
         </div>
       </div>
-    </div>)
+    </div>
+  )
 }
